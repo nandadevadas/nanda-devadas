b19e579ab9deab5d077a0761927068074d6572ad
Nanda Dev <nandadevadas@gmail.com>
2025-10-01 01:48:50 +0000

    write this in one line (_for element p_)

--- src/components/portfolio-section.tsx
+++ src/components/portfolio-section.tsx
@@ -43,7 +43,7 @@ export default function PortfolioSection() {
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
             <h2 className="text-3xl font-headline font-bold tracking-tighter sm:text-5xl">Evidence of Practice</h2>
-            <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+            <p className="text-muted-foreground">
               Explore what learning looked like and why it worked in my classroom & here is “A quick view of what I bring to your school.”
             </p>
           </div>
