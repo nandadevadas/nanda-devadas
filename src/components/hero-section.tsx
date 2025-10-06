90210bd83382049565cde10174d9ab1e6a520e9d
Nanda Dev <nandadevadas@gmail.com>
2025-10-06 00:17:27 +0000

    upon clicking on these download resume buttons throughout the page it sh

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -45,7 +45,7 @@ export default function HeroSection() {
                 </Link>
               </Button>
               <Button asChild variant="outline" size="lg">
-                <Link href="/assets/resume/Resume_Nanda_MyKelly.pdf" download>Download Resume</Link>
+                <Link href="/assets/resume/Resume_Nanda_Dev.pdf" download>Download Resume</Link>
               </Button>
             </div>
           </div>
